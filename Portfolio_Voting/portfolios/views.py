5d7c6137c21f55d4d23d363fc5a9561ce2986a2a
Sam Yang <samabc75@gmail.com>
2020-07-08 01:33:40 +0800

    update voting, add a warning modal before vote

--- Portfolio_Voting/portfolios/views.py
+++ Portfolio_Voting/portfolios/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib import messages
@@ -8,15 +8,37 @@ from portfolios.models import Work, UserVote
 # Create your views here.
 def index(request, college):
 	work_list = Work.objects.filter(work_college=college).all()
+	StudVoted = len(UserVote.objects.filter(studID=request.user.username).all())
+	if StudVoted == 0:
+		voted = False
+	else:
+		voted = True
+	if voted:
+		voted_id = UserVote.objects.get(studID=request.user.username).choice_workTitle.id
+	else:
+		voted_id = -1
+
 	context = {
 		'work_list': work_list,
-		'college': college
+		'college': college,
+		'voted': voted,
+		'voted_id': voted_id
 	}
 	return render(request, 'portfolios/index.html', context)
 
 def article(request, portfolio_id):
 	work = get_object_or_404(Work, pk=portfolio_id)
-	return render(request, 'portfolios/article.html', {'work': work})
+	StudVoted = len(UserVote.objects.filter(studID=request.user.username).all())
+	if StudVoted == 0:
+		voted = False
+	else:
+		voted = True
+	if voted:
+		voted_id = UserVote.objects.get(studID=request.user.username).choice_workTitle.id
+	else:
+		voted_id = -1
+
+	return render(request, 'portfolios/article.html', {'work': work, 'voted': voted, 'voted_id': voted_id})
 
 def results(request, choiceText_id):
 	work = get_object_or_404(Work, pk=choiceText_id)
@@ -31,6 +53,10 @@ def results(request, choiceText_id):
 
 
 def vote(request, portfolio_id):
+	if not request.user.is_authenticated:
+		messages.add_message(request, messages.WARNING, '請先登入再進行投票')
+		return redirect('users:login')
+
 	work = get_object_or_404(Work, pk=portfolio_id)
 
 	StudVoted = len(UserVote.objects.filter(studID=request.user.username).all())

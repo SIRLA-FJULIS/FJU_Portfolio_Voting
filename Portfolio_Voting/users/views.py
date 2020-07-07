ffb552b7fdceb292631bbd9c580c2a55a21286fe
Sam Yang <samabc75@gmail.com>
2020-07-07 22:23:32 +0800

    move users related link to top

--- Portfolio_Voting/users/views.py
+++ Portfolio_Voting/users/views.py
@@ -1,23 +1,25 @@
 from django.shortcuts import render, redirect
 from django.contrib import auth
 from django.urls import reverse
+from django.contrib import messages
 
 from users.models import User
 from users.forms import RegisterForm
 
 # Create your views here.
 def login(request):
-	username = request.POST.get('username')
-	password = request.POST.get('password')
-	user = auth.authenticate(username=username, password=password)
+	if request.POST:
+		username = request.POST.get('username')
+		password = request.POST.get('password')
+		user = auth.authenticate(username=username, password=password)
 
-	if user is not None and user.is_active:
-		auth.login(request, user)
-		print('pass', username)
-		return redirect(reverse('index'))
-	else:
-		print('no pass', username)
-		return render(request, 'registration/login.html', locals())    
+		if user is not None and user.is_active:
+			auth.login(request, user)
+			return redirect(reverse('index'))
+		else:
+			messages.add_message(request, messages.ERROR, '學號或密碼錯誤')
+	
+	return render(request, 'registration/login.html', locals())    
 
 
 def logout(request):

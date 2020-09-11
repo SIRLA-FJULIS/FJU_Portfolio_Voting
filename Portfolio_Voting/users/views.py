c9b4b26d5457b76b8c4c116ff5b6d3e3aec2ead5
root <root@hecct.hecct.fju.edu.tw>
2020-09-11 15:07:56 +0800

    (Complete) verify FJU LDAP and write back UserLogin_info to database, then auto login.

--- Portfolio_Voting/users/views.py
+++ Portfolio_Voting/users/views.py
@@ -5,59 +5,60 @@ from django.contrib import messages
 
 from users.models import User
 
-from django.http import HttpResponseRedirect, HttpResponse, HttpResponsePermanentRedirect
-import json
-
+from django.http import HttpResponseRedirect, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 
 # Create your views here.
+@csrf_exempt
 def login(request):
-	if request.POST:
-		username = request.POST.get('id')
-		password = request.POST.get('pasw')
-
-		USER_EXIST = len(User.objects.all().filter(username=username))
-		print(USER_EXIST)
-		if USER_EXIST == 1:
-			user = auth.authenticate(username=username, password=password)
-			if user is not None and user.is_active:
-				auth.login(request, user)
-				return redirect(reverse('index'))
-			else:
-				messages.add_message(request, messages.ERROR, '學號或密碼錯誤')
-		else:
-			request.session['INPUT_STUDID'] = username
-			request.session['INPUT_PASSWORD'] = password
-			return HttpResponse(request)
-				#redirect('verify')
-
-			#return HttpResponse('{}\n==>\n'.format(request.POST))
-
-#			NEW_USER_RECORD = User.objects.create(username=username, is_active=True, is_admin=False)
-#			NEW_USER_RECORD.set_password(password)
-#			NEW_USER_RECORD.save()
-#			print(NEW_USER_RECORD)
-
-#			user = auth.authenticate(username=username, password=password)
-#			auth.login(request, user)
-#			return redirect(reverse('index'))
-
-
-
-#			if verify(username, password) == True:
-#				First, add user record to database.
-#				Second, login this poll website.
-#			else:
-#				SHOW messages.ERROR
-
-	return render(request, 'registration/login.html', locals())
-
+    if request.method == 'GET':
+        return render(request, 'registration/login.html', locals())
+    elif request.method == 'POST':
+        username = request.POST.get('id')
+        password = request.POST.get('pasw')
+        #print(username, password)
+        request.session['INPUT_STUDID'] = username
+        request.session['INPUT_PASSWORD'] = password
+        return redirect(reverse('users:verify'))
 
 def logout(request):
     auth.logout(request)
     return redirect(reverse('users:login'))
 
-
+@csrf_exempt
 def verify(request):
-    response = HttpResponse(request)
-    return render(request, 'test.html', {'id':id, 'type':type, 'state':state, 'response':response})
+    username = request.session['INPUT_STUDID']
+    password = request.session['INPUT_PASSWORD']
+    #print(password, username)
+
+    id = request.POST['id']
+    type = request.POST['type']
+    state = request.POST['state']
+
+    if type == '1' and state == '1':
+        NEW_USER_RECORD = User.objects.create(username=username, is_active=True, is_admin=False)
+        NEW_USER_RECORD.set_password(password)
+        NEW_USER_RECORD.save()
+        #print(NEW_USER_RECORD)
+        user = auth.authenticate(username=username, password=password)
+        auth.login(request, user)
+        return redirect(reverse('index'))
+        #return render(request, 'test.html', {'id':id, 'type':type, 'state':state, 'username':username, 'password':password})
+
+    elif type == '2' and state == '1':
+        NEW_USER_RECORD = User.objects.create(username=username, is_active=True, is_admin=False)
+        NEW_USER_RECORD.set_password(password)
+        NEW_USER_RECORD.save()
+        #print(NEW_USER_RECORD)
+        user = auth.authenticate(username=username, password=password)
+        auth.login(request, user)
+        return redirect(reverse('index'))
+        #return render(request, 'test.html', {'id':id, 'type':type, 'state':state, 'username':username, 'password':password})
+
+    else:
+        return HttpResponse('Login Error!')
+    #NEW_USER_RECORD = User.objects.create(username=username, is_active=True, is_admin=False)
+    #NEW_USER_RECORD.set_password(password)
+    #NEW_USER_RECORD.save()
+    #print(NEW_USER_RECORD)
